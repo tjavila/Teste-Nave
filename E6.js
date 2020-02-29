7f6e16b6166782a4a3b20cfbed7973830a249408
Tayssa Avila <57669813+tjavila@users.noreply.github.com>
2020-02-28 21:13:06 -0300

    Create E6.js

---
+++ E6.js
@@ -0,0 +1,5 @@
+let numero = 45;
+
+axios.get('https://viacep.com.br/ws/96215420/json/').then(function(res){
+  console.log(res.data.logradouro.toUpperCase() + ', ' + numero + ", " + res.data.localidade.toUpperCase() + "/" + res.data.uf.toUpperCase());
+})
