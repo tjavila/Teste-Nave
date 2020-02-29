76bf0196bc2f2a8d25adb54f852bec080828751b
Tayssa Avila <57669813+tjavila@users.noreply.github.com>
2020-02-28 21:35:47 -0300

    Create E10.js

---
+++ E10.js
@@ -0,0 +1,12 @@
+let objetos = [
+    {id: 1, nome: 'juca', sobrenome: 'da silva', idade: 42},
+    {id: 2, nome: 'daniel', sobrenome: 'gonçalves',  idade: 21},
+    {id:3, nome: 'matheus', sobrenome: 'garcia', idade: 28},
+    {id: 4, nome: 'gabriel', sobrenome: 'pinheiro',  idade: 21}
+]
+
+objetos.forEach(function(atual) {
+  if(atual.idade<30){
+    console.log(atual)
+  }
+});
