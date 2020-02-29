6e383fa5db0eca46329eaa4a89f9172ce233905f
Tayssa Avila <57669813+tjavila@users.noreply.github.com>
2020-02-28 21:28:57 -0300

    Create E8.js

---
+++ E8.js
@@ -0,0 +1,10 @@
+let objetos = [
+    {id: 1, nome: 'juca', sobrenome: 'da silva', idade: 42},
+    {id: 2, nome: 'daniel', sobrenome: 'gonçalves',  idade: 21},
+    {id:3, nome: 'matheus', sobrenome: 'garcia', idade: 28},
+    {id: 4, nome: 'gabriel', sobrenome: 'pinheiro',  idade: 21}
+]
+
+let somaIdades = objetos.reduce((soma, atual) => soma + atual.idade, 0);
+
+console.log(somaIdades);
